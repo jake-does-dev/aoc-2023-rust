2b4264c553f201c238aec1167fac7014a4731bff
Jake Allan <jjemail1995@gmail.com>
2023-12-03 14:45:11 +0000

    day03

--- src/day03.rs
+++ src/day03.rs
@@ -1,5 +1,6 @@
+use std::fmt::Debug;
+
 use regex::Regex;
-use std::fmt::{Debug, Formatter};
 
 use crate::{DayRunner, Part};
 
@@ -12,35 +13,16 @@ impl DayRunner for Day03 {
             Part::PartTwo => part_two(include_str!("inputs/day03_puzzle.txt")),
         };
 
-        Self::report_result("day01", part, result);
+        Self::report_result("day03", part, result);
     }
 }
 
-fn numbers_pattern() -> Regex {
-    Regex::new(r"(\d+)+").unwrap()
-}
-
-fn symbols_pattern() -> Regex {
-    Regex::new(r"[^\d.\n]").unwrap()
-}
-
-// #[derive(Debug)]
 struct Number {
     row_index: usize,
     column_start_index: usize,
     column_end_index: usize,
     value: u32,
     visited: bool,
-}
-
-impl Debug for Number {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{} @ ({},{}-{})",
-            self.value, self.row_index, self.column_start_index, self.column_end_index
-        )
-    }
 }
 
 impl Number {
@@ -68,7 +50,6 @@ impl Number {
 struct Symbol {
     row_index: usize,
     column_index: usize,
-    value: String,
 }
 
 impl Symbol {
@@ -101,38 +82,48 @@ impl Symbol {
         }
 
         visited_numbers
-
-        // let smallest_row_index = if number.row_index == 0 {
-        //     0
-        // } else {
-        //     number.row_index - 1
-        // };
-        // let largest_row_index = number.row_index + 1;
-        // let smallest_column_index = if number.column_start_index == 0 {
-        //     0
-        // } else {
-        //     number.column_start_index - 1
-        // };
-        // let largest_column_index = number.column_end_index + 1;
-        //
-        // if self.row_index >= smallest_row_index
-        //     && self.row_index <= largest_row_index
-        //     && self.column_index >= smallest_column_index
-        //     && self.column_index <= largest_column_index
-        // {
-        //     number.visit();
-        // }
     }
 }
 
 fn part_one(data: &str) -> u32 {
-    let lines: Vec<&str> = data.lines().collect();
+    let mut numbers = extract_numbers(data, &Regex::new(r"(\d+)+").unwrap());
+    let symbols = extract_symbols(data, &Regex::new(r"[^\d.\n]").unwrap());
 
+    for symbol in &symbols {
+        symbol.visit(&mut numbers);
+    }
+
+    numbers
+        .iter()
+        .filter(|number| number.visited)
+        .map(|number| number.value)
+        .sum()
+}
+
+#[allow(clippy::trivial_regex)]
+fn part_two(data: &str) -> u32 {
+    let mut numbers = extract_numbers(data, &Regex::new(r"(\d+)+").unwrap());
+    let symbols = extract_symbols(data, &Regex::new(r"[*]").unwrap());
+
+    let mut gear_part_ratios: Vec<u32> = vec![];
+
+    for symbol in &symbols {
+        let visited_numbers = symbol.visit(&mut numbers);
+        if visited_numbers.len() == 2 {
+            gear_part_ratios.push(
+                visited_numbers.first().unwrap().value * visited_numbers.last().unwrap().value,
+            );
+        }
+    }
+
+    gear_part_ratios.iter().sum()
+}
+
+fn extract_numbers(data: &str, numbers_pattern: &Regex) -> Vec<Number> {
     let mut numbers: Vec<Number> = vec![];
-    let mut symbols: Vec<Symbol> = vec![];
 
-    lines.iter().enumerate().for_each(|(row_index, line)| {
-        numbers_pattern().captures_iter(line).for_each(|captures| {
+    data.lines().enumerate().for_each(|(row_index, line)| {
+        numbers_pattern.captures_iter(line).for_each(|captures| {
             let number = captures.get(0).unwrap();
             let (column_start_index, column_end_index, value) = (
                 number.start(),
@@ -142,46 +133,30 @@ fn part_one(data: &str) -> u32 {
             numbers.push(Number::new(
                 row_index,
                 column_start_index,
-                column_end_index,
+                column_end_index - 1, //the Regex crate has the end index as exclusive
                 value,
             ));
         });
+    });
 
-        symbols_pattern().captures_iter(line).for_each(|captures| {
+    numbers
+}
+
+fn extract_symbols(data: &str, symbols_pattern: &Regex) -> Vec<Symbol> {
+    let mut symbols: Vec<Symbol> = vec![];
+
+    data.lines().enumerate().for_each(|(row_index, line)| {
+        symbols_pattern.captures_iter(line).for_each(|captures| {
             let symbol = captures.get(0).unwrap();
-            let (column_index, value) = (symbol.start(), symbol.as_str().to_string());
+            let column_index = symbol.start();
             symbols.push(Symbol {
                 row_index,
                 column_index,
-                value,
             });
         });
     });
 
-    symbols.iter().for_each(|symbol| {
-        let visited_numbers = symbol.visit(&mut numbers);
-        println!(
-            "The symbol {} at position ({},{}) has visited the following numbers: {:?}",
-            symbol.value, symbol.row_index, symbol.column_index, visited_numbers
-        );
-    });
-
-    // let not_visited: Vec<&Number> = numbers.iter().filter(|number| !number.visited).collect();
-
-    // println!("not_visited: {not_visited:#?}");
-
-    numbers
-        .iter()
-        .filter(|number| number.visited)
-        .map(|number| {
-            // println!("visited number: {number:?}");
-            number.value
-        })
-        .sum()
-}
-
-fn part_two(_data: &str) -> u32 {
-    2
+    symbols
 }
 
 #[cfg(test)]
@@ -189,24 +164,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part_one_example_input() {
+    fn day03_part_one_example_input() {
         assert_eq!(part_one(include_str!("inputs/day03_example.txt"),), 4361);
     }
 
     #[test]
-    fn part_one_puzzle_input() {
-        assert_eq!(part_one(include_str!("inputs/day03_puzzle.txt"),), 2);
+    fn day03_part_one_puzzle_input() {
+        assert_eq!(part_one(include_str!("inputs/day03_puzzle.txt"),), 556_057);
     }
 
     #[test]
-    #[ignore]
-    fn part_two_example_input() {
-        assert_eq!(part_two(include_str!("inputs/day03_example.txt"),), 2286);
+    fn day03_part_two_example_input() {
+        assert_eq!(part_two(include_str!("inputs/day03_example.txt"),), 467_835);
     }
 
     #[test]
-    #[ignore]
-    fn part_two_puzzle_input() {
-        assert_eq!(part_two(include_str!("inputs/day03_puzzle.txt"),), 63542);
+    fn day03_part_two_puzzle_input() {
+        assert_eq!(
+            part_two(include_str!("inputs/day03_puzzle.txt"),),
+            82_824_352
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use aoc_2023_rust::day01::Day01;
 use aoc_2023_rust::day02::Day02;
+use aoc_2023_rust::day03::Day03;
 use aoc_2023_rust::{DayRunner, Part};
 
 fn main() {
@@ -7,4 +8,6 @@ fn main() {
     Day01::run(Part::PartTwo);
     Day02::run(Part::PartOne);
     Day02::run(Part::PartTwo);
+    Day03::run(Part::PartOne);
+    Day03::run(Part::PartTwo);
 }
